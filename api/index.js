358943d1b8e1c4a6c3cccf3b3ce785753c13f111
Richard Armuelles <alexanderarmuelles@riseup.net>
2023-07-11 10:58:42 -0500

    agregando path a index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import { ApolloServer, gql } from "apollo-server-express";
 import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import path from "path";
 import http from "http";
 import express from "express";
 import cors from "cors";
